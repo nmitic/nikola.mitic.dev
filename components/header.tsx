64f70a3c2e4d174ae45fb441035b856fa7ec9821
Nikola Mitic <nikola.mitic.dev@gmail.com>
2023-05-02 13:03:56 +0200

    fixed tailwind imports and styled Logo icon

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import logoIcon from "../public/logo.svg";
+import logoIcon from "../public/logo-inverted.svg";
 
 const Header = () => {
   return (
-    <header>
-      <Image priority src={logoIcon} alt="nikola.mitic.dev logo" />
+    <header className="container mx-auto px-4 py-4">
+      <Image priority src={logoIcon} alt="nikola.mitic.dev logo" width={200} />
     </header>
   );
 };
