a8554850606ae7a14197b36944e739ad701de161
Nikola Mitic <nikola.mitic.dev@gmail.com>
2023-05-02 13:24:00 +0200

    Make logo icon link to home page

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
 import logoIcon from "../public/logo-inverted.svg";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="container mx-auto px-4 py-4">
-      <Image priority src={logoIcon} alt="nikola.mitic.dev logo" width={200} />
+      <Link href="/">
+        <Image
+          priority
+          src={logoIcon}
+          alt="nikola.mitic.dev logo"
+          width={200}
+        />
+      </Link>
     </header>
   );
 };
