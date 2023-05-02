33a30774a300c0d4d318cf3ba7398b02f808491a
Nikola Mitic <nikola.mitic.dev@gmail.com>
2023-05-02 23:28:59 +0200

    added footer with social links

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
 import logoIcon from "../public/logo-inverted.svg";
+import SoLogoIcon from "../public/so-logo.svg";
+import CodepenLogoIcon from "../public/codepen-logo.svg";
+import GithubLogo from "../public/GitHub_Logo_White.png";
+import LinkedinLogo from "../public/LI-Logo.png";
 import "../styles/globals.css";
 
 export default function RootLayout({
@@ -11,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div className="grid h-screen auto-rows-[auto_1fr] container mx-auto p-4">
+        <div className="grid h-screen grid-rows-[auto,1fr] container mx-auto p-4">
           <header>
             <Link href="/">
               <Image
@@ -23,6 +27,62 @@ export default function RootLayout({
             </Link>
           </header>
           <main>{children}</main>
+          <footer>
+            <ul className="text-right">
+              <li>
+                <a
+                  href="https://github.com/nmitic"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Image
+                    className=" w-20 aspect-[5/2] object-contain inline-block"
+                    src={GithubLogo}
+                    alt="Github logo"
+                  />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://stackoverflow.com/users/5794392/nikola-mitic"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Image
+                    src={SoLogoIcon}
+                    alt="stack overflow icon"
+                    className=" w-20 aspect-[5/2] object-contain inline-block"
+                  />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://codepen.io/nikolamitic"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Image
+                    className=" w-20 aspect-[5/2] object-contain inline-block"
+                    src={CodepenLogoIcon}
+                    alt="codepen logo"
+                  />
+                </a>
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.linkedin.com/in/nikola-mitic-6a5b11119/"
+                >
+                  <Image
+                    className=" w-20 aspect-[5/2] object-contain inline-block"
+                    src={LinkedinLogo}
+                    alt="Linkedin logo"
+                  />
+                </a>
+              </li>
+            </ul>
+          </footer>
         </div>
       </body>
     </html>
