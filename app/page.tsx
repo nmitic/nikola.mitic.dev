852036a91eb78eb5df7fe3d0a82d4cae72b29c2f
Nikola Mitic <nikola.mitic.dev@gmail.com>
2023-05-04 09:30:12 +0200

    Make root page's menu more semantic

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,25 @@ import Link from "next/link";
 export default function Home() {
   return (
     <section className="flex h-full justify-center items-center">
-      <ul className="leading-10">
-        <li>
-          <Link href="/blog" className="text-6xl hover:underline">
-            BLOG
-          </Link>
-        </li>
-        <li>
-          <Link href="/portfolio" className="text-6xl hover:underline">
-            PORTFOLIO
-          </Link>
-        </li>
-        <li>
-          <Link href="/cv" className="text-6xl hover:underline">
-            CV
-          </Link>
-        </li>
-      </ul>
+      <nav>
+        <ul className="leading-10">
+          <li>
+            <Link href="/blog" className="text-6xl hover:underline">
+              BLOG
+            </Link>
+          </li>
+          <li>
+            <Link href="/portfolio" className="text-6xl hover:underline">
+              PORTFOLIO
+            </Link>
+          </li>
+          <li>
+            <Link href="/cv" className="text-6xl hover:underline">
+              CV
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </section>
   );
 }
