8bf7713420679e99b55abc5846724188d01d284e
Nikola Mitic <nikola.mitic.dev@gmail.com>
2023-05-29 20:52:00 +0200

    generating pdf on CI test

--- cv-to-pdf.ts
+++ cv-to-pdf.ts
@@ -4,7 +4,7 @@ const { chromium } = require('@playwright/test');
   const browser = await chromium.launch();
   const context = await browser.newContext();
   const page = await context.newPage();
-  await page.goto('http://localhost:3000/cv');
+  await page.goto('https://nikola-mitic-dev.vercel.app/cv');
   await page.pdf({ path: `./public/nikola_mitic_cv.pdf`, printBackground: true });
   await browser.close();
 })();
