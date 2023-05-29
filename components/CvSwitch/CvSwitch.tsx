89a2cec988b1a88035874425dd5315480d696716
Nikola Mitic <nikola.mitic.dev@gmail.com>
2023-05-29 14:20:59 +0200

    Fixed SvSwitch bug on CV entry page

--- components/CvSwitch/CvSwitch.tsx
+++ components/CvSwitch/CvSwitch.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
 
 const CvSwitch = ({ defaultChecked = true }: { defaultChecked?: boolean }) => {
   const router = useRouter();
+  const pathName = usePathname();
   const [isChecked, setIsChecked] = useState(defaultChecked);
+  const isNotOnCvEntryPath = pathName !== "/cv";
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.checked) {
@@ -20,10 +22,9 @@ const CvSwitch = ({ defaultChecked = true }: { defaultChecked?: boolean }) => {
     <label className="relative inline-flex items-center cursor-pointer">
       <input
         type="checkbox"
-        value="checked"
         className="sr-only peer"
         onChange={handleChange}
-        checked={isChecked}
+        checked={isChecked && isNotOnCvEntryPath}
       />
       <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-black after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-gray"></div>
       <span className="ml-3 text-sm font-medium uppercase">
