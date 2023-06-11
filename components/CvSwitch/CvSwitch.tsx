ccc7dc4ffad83eb56811d6d0c79dfa3bae9166e2
Nikola Mitic <nikola.mitic.dev@gmail.com>
2023-06-11 22:34:33 +0200

    fixed CvSwitch delay in state change

--- components/CvSwitch/CvSwitch.tsx
+++ components/CvSwitch/CvSwitch.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
 
 const CvSwitch = ({
   onSwitch,
@@ -9,16 +10,15 @@ const CvSwitch = ({
 }) => {
   const router = useRouter();
   const pathName = usePathname();
-  const isChecked = pathName !== "/cv";
+  const [isSwitched, setIsSwitched] = useState(pathName !== "/cv");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const isSwitched = e.target.checked;
-
+    setIsSwitched(!isSwitched);
     if (onSwitch) {
       onSwitch(isSwitched);
     }
 
-    if (isSwitched) {
+    if (!isSwitched) {
       router.back();
       return;
     }
@@ -32,7 +32,7 @@ const CvSwitch = ({
         type="checkbox"
         className="sr-only peer"
         onChange={handleChange}
-        checked={isChecked}
+        checked={isSwitched}
       />
       <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-black after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-gray"></div>
       <span className="ml-3 text-sm font-medium uppercase">
