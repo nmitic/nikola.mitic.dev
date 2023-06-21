53cc5cdf62b69f32bba5541dd20eeada5b7c16fa
Nikola Mitic <nikola.mitic.dev@gmail.com>
2023-06-21 14:05:37 +0200

    Update page.tsx

--- app/tiny_thoughts/page.tsx
+++ app/tiny_thoughts/page.tsx
@@ -19,7 +19,7 @@ const client = new GraphQLClient(
 const TinyThoughts = async () => {
   const query = gql`
     query TinyThoughtsQuery {
-      tinyThoughts(orderBy: createdAt_ASC) {
+      tinyThoughts(orderBy: createdAt_DESC) {
         id
         createdAt
         content {
