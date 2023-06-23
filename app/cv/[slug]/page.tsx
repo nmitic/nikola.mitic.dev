f5adb7f9f86d6def03f75361fd436cee752da40a
Nikola Mitic <nikola.mitic.dev@gmail.com>
2023-06-23 12:56:37 +0200

    Fetched variant and render it

--- app/cv/[slug]/page.tsx
+++ app/cv/[slug]/page.tsx
@@ -1,5 +1,10 @@
 import { GraphQLClient, gql } from "graphql-request";
-import { JobData } from "../../../types/cv";
+import {
+  JobData,
+  SplitTestingsData,
+  JobWithSplitTesting,
+  Job,
+} from "../../../types/cv";
 import { JobView } from "../../../components/JobView";
 
 const client = new GraphQLClient(
@@ -8,9 +13,22 @@ const client = new GraphQLClient(
 
 const JobPage = async ({ params }: { params: { slug: string } }) => {
   const slug = params.slug;
-  const query = gql`
+  const querySPLIT_TESTINGS = gql`
+    query GetSplitTestings {
+      splitTestings {
+        id
+        name
+        enableSplitTesting
+        splittingTestingResource {
+          id
+        }
+      }
+    }
+  `;
+  const queryJOB = gql`
     query GetJobBySlug($slug: String) {
       job(where: { slug: $slug }) {
+        id
         description {
           markdown
         }
@@ -25,14 +43,49 @@ const JobPage = async ({ params }: { params: { slug: string } }) => {
         themeColor {
           hex
         }
+        splitTesting {
+          variantName
+          weight
+          variantResource {
+            description {
+              markdown
+            }
+            location
+            companyName
+            companyWebsite
+            endDate
+            title
+            startDate
+            industry
+            techStackTools
+            themeColor {
+              hex
+            }
+          }
+        }
       }
     }
   `;
-  const data: JobData = await client.request(query, { slug });
+  const data: JobData<JobWithSplitTesting> = await client.request(queryJOB, {
+    slug,
+  });
+  const splitTestingData: SplitTestingsData = await client.request(
+    querySPLIT_TESTINGS,
+    { slug }
+  );
+  const hasRunningSplitTest = splitTestingData.splitTestings.some(
+    (item) =>
+      item.enableSplitTesting &&
+      item.splittingTestingResource.id === data.job.id
+  );
+
+  const JobData = hasRunningSplitTest
+    ? data.job.splitTesting[0].variantResource
+    : data.job;
 
   return (
     <section>
-      <JobView job={data.job} />
+      <JobView job={JobData} />
     </section>
   );
 };
@@ -68,7 +121,7 @@ export const generateMetadata = async ({
       startDate,
       industry,
     },
-  }: JobData = await client.request(query, { slug });
+  }: JobData<Job> = await client.request(query, { slug });
 
   return {
     title: `Nikola Mitic - ${companyName} - ${location} - ${title} - ${industry}`,
--- types/cv.ts
+++ types/cv.ts
@@ -6,7 +6,7 @@ export type Job = {
   companyWebsite: string
   endDate: string
   slug: string
-  location:string
+  location: string
   title: string
   startDate: string
   id: number
@@ -17,10 +17,62 @@ export type Job = {
   }
 }
 
-export type JobData = {
-  job: Job
+export type JobWithSplitTesting = {
+  description: {
+    markdown: string
+  },
+  companyName: string
+  companyWebsite: string
+  endDate: string
+  slug: string
+  location: string
+  title: string
+  startDate: string
+  id: number
+  industry: string
+  techStackTools: string[]
+  themeColor: {
+    hex: string
+  }
+  splitTesting: SplitTestingOnJob
+}
+
+export type SplitTestingOnJob = {
+  variantResource: {
+    description: {
+      markdown: string
+    },
+    companyName: string
+    companyWebsite: string
+    endDate: string
+    slug: string
+    location: string
+    title: string
+    startDate: string
+    id: number
+    industry: string
+    techStackTools: string[]
+    themeColor: {
+      hex: string
+    }
+  }
+}[]
+
+export type JobData<T> = {
+  job: T
 }
 
 export type JobsData = {
   jobs: Job[]
+}
+
+export type SplitTesting = {
+  id: number
+  name: string
+  enableSplitTesting: boolean
+  splittingTestingResource: Job
+}
+
+export type SplitTestingsData = {
+  splitTestings: SplitTesting[]
 }
