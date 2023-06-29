877b88f194cce231d4aa767a209f8329ba1971b1
Nikola Mitic <nikola.mitic.dev@gmail.com>
2023-06-29 10:14:45 +0200

    added animation to menu items

--- components/MenuItem.tsx
+++ components/MenuItem.tsx
@@ -1,15 +1,23 @@
+"use client";
+
+import { motion } from "framer-motion";
 import Link from "next/link";
 
 const MenuItem = ({ href, text }: { href: string; text: string }) => {
   return (
-    <li className="group relative">
+    <motion.li
+      initial={{ opacity: 0, scale: 1.4 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0.6 }}
+      className="group relative"
+    >
       <Link
         href={href}
         className="text-6xl group-hover:translate-x-8 transition w-full block group-hover:underline"
       >
         {text}
       </Link>
-    </li>
+    </motion.li>
   );
 };
 
