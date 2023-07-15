d6d538bedd36113100e2f1defcf86f8e3a861d52
Nikola Mitic <nikola.mitic.dev@gmail.com>
2023-07-15 16:03:50 +0200

    CV: Added download CV back

--- components/DownloadCv.tsx
+++ components/DownloadCv.tsx
@@ -2,13 +2,12 @@
 
 export const DownloadCvLink = () => {
   return (
-    <button
+    <a
       className="print:hidden bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow self-start inline-block"
-      onClick={() => {
-        window.print();
-      }}
+      href="/nikola_mitic_cv.pdf"
+      download="nikola_mitic_cv"
     >
-      Print or save
-    </button>
+      Download CV
+    </a>
   );
 };
