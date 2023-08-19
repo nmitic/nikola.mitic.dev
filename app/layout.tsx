8143fdee0ef9e6ec8dbf59e02f24879b9b01e3c5
nmitic <nikola.mitic.dev@gmail.com>
2023-08-19 16:01:40 +0200

    removes not needed imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,8 @@
-import Link from "next/link";
 import { Analytics } from "@vercel/analytics/react";
-import Image from "next/image";
-import logoIcon from "../public/logo-inverted.svg?url";
 import Footer from "../components/footer";
 import { footerLinks } from "../static-data/footer-links";
 
 import "../styles/globals.css";
-import { ProfileImage } from "../components/ProfileImage";
 import { AuthProvider } from "../context/AuthProvider";
 import { Header } from "../components/Header";
 
