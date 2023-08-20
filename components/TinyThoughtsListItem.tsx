238b490a99ccc7a7dd2befdb8680893133d34c35
nmitic <nikola.mitic.dev@gmail.com>
2023-08-20 21:03:11 +0200

    adds support for order list

--- components/TinyThoughtsListItem.tsx
+++ components/TinyThoughtsListItem.tsx
@@ -4,7 +4,7 @@ import Markdown from "markdown-to-jsx";
 import { ClientDate } from "./ClientDate";
 import Image from "next/image";
 import profilePhoto from "../public/profile_photo.jpeg";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { tinyThought } from "../types/tt";
 import { useSession } from "next-auth/react";
 
@@ -24,6 +24,21 @@ declare module "slate" {
   }
 }
 
+const ParagraphElement = (props: any) => {
+  return <p {...props.attributes}>{props.children}</p>;
+};
+
+// Define a React component renderer for our code blocks.
+const NumberedList = (props: any) => {
+  return (
+    <ol {...props.attributes}>
+      {props.children.map((item: any) => (
+        <li>{item}</li>
+      ))}
+    </ol>
+  );
+};
+
 export const TinyThoughtsListItem = ({
   tinyThought,
 }: {
@@ -33,6 +48,19 @@ export const TinyThoughtsListItem = ({
   const { data: session } = useSession();
 
   const [editor] = useState(() => withReact(createEditor()));
+
+  // Define a rendering function based on the element passed to `props`. We use
+  // `useCallback` here to memoize the function for subsequent renders.
+  const renderElement = useCallback((props: any) => {
+    switch (props.element.type) {
+      case "paragraph":
+        return <ParagraphElement {...props} />;
+      case "numbered-list":
+        return <NumberedList>{props.children}</NumberedList>;
+      default:
+        return <ParagraphElement {...props} />;
+    }
+  }, []);
 
   const isLoggedIn = !!session;
 
@@ -54,12 +82,14 @@ export const TinyThoughtsListItem = ({
         </section>
         <section className="prose prose-invert max-w-none mb-4">
           {editMode ? (
-            <Slate
-              editor={editor}
-              initialValue={tinyThought.content.raw.children}
-            >
-              <Editable />
-            </Slate>
+            <div className="text-white">
+              <Slate
+                editor={editor}
+                initialValue={tinyThought.content.raw.children}
+              >
+                <Editable renderElement={renderElement} />
+              </Slate>
+            </div>
           ) : (
             <Markdown className="text-white">
               {tinyThought.content.markdown}
