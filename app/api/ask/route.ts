4cd652453f0cf6688f650f138c62da6c9c34d1d5
Nikola Mitic <nikola.mitic.dev@gmail.com>
2024-01-14 00:26:40 +0100

    makes use of hygraph data for llama documents

--- app/api/ask/route.ts
+++ app/api/ask/route.ts
@@ -1,18 +1,45 @@
 // example.ts
 import fs from "fs/promises";
-import { Document, VectorStoreIndex } from "llamaindex";
+import { GraphQLClient, gql } from "graphql-request";
+import { Document, VectorStoreIndex, jsonToNode, ObjectType } from "llamaindex";
 import { NextResponse } from "next/server";
+
+const client = new GraphQLClient(
+  process.env.NEXT_PUBLIC_HYGRAPH_READ_ONLY as string
+);
+
+const jobsQuery = gql`
+  query Jobs {
+    page(where: {slug: "tiny-thoughts"}) {
+      description {
+        text
+      }
+    }
+    jobs {
+      description {
+        text
+      }
+      companyName
+      endDate
+      startDate
+      techStackTools
+      title
+    }
+    tinyThoughts(first: 100) {
+    content {
+      text
+    }
+  }
+  }
+`
 
 // To handle a GET request to /api
 export async function POST(request: Request) {
-  // Load essay from abramov.txt in Node
-  const essay = await fs.readFile(
-    "node_modules/llamaindex/examples/abramov.txt",
-    "utf-8",
-  );
+  // load jobs data
+  const data = await client.request(jobsQuery)
 
   // Create Document object with essay
-  const document = new Document({ text: essay });
+  const document = new Document({ text: JSON.stringify(data) });
 
   // Split text and create embeddings. Store them in a VectorStoreIndex
   const index = await VectorStoreIndex.fromDocuments([document]);
@@ -20,9 +47,9 @@ export async function POST(request: Request) {
   // Query the index
   const queryEngine = index.asQueryEngine();
   const response = await queryEngine.query(
-    "What did the author do in college?",
+    "What are Nikola's biggest frustration and how is he solving them?",
   );
 
-  // Do whatever you want
+
   return NextResponse.json({ response: response.response }, { status: 200 });
 }
