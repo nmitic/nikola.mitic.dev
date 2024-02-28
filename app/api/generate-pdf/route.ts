9af957c3b585e19a80d917e74b8db788303eadf0
nmitic <nikola.mitic.dev@gmail.com>
2024-02-28 22:54:20 +0100

    adds puputer to deal with pdf generation via route handler

---
+++ app/api/generate-pdf/route.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+import puppeteer from "puppeteer";
+
+async function printPDF() {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto(process.env.PDF_GENERATION_PATH as string, {
+    waitUntil: "networkidle0",
+  });
+  const pdf = await page.pdf({ format: "A3", printBackground: true });
+
+  await browser.close();
+  return pdf;
+}
+
+export async function GET() {
+  try {
+    const pdf = await printPDF();
+
+    const response = new NextResponse(pdf);
+    response.headers.set("content-type", "application/pdf");
+
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+}
