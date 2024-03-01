2634dd3d67bf4d99e8f140a7143cb8e4d505d72d
nmitic <nikola.mitic.dev@gmail.com>
2024-03-01 10:13:48 +0100

    renames pdf route

--- components/DownloadCv.tsx
+++ components/DownloadCv.tsx
@@ -2,7 +2,7 @@ export const DownloadCvLink = () => {
   return (
     <a
       className="print:hidden bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow self-start inline-block"
-      href="/api/generate-pdf"
+      href="/api/nikola_mitic_cv"
       target="_blank"
     >
       Download CV
