521e29874c2d79926011bbd60c07d819200d37d2
nmitic <nikola.mitic.dev@gmail.com>
2024-05-09 11:12:47 +0200

    Adds color and icon coding to talk

--- components/InterviewerAITalk/components/TalkStatus.tsx
+++ components/InterviewerAITalk/components/TalkStatus.tsx
@@ -1,3 +1,8 @@
+import Microphone from "../../../public/microphone.svg";
+import MicrophoneDisabled from "../../../public/microphone-disabled.svg";
+import Voice from "../../../public/voice.svg";
+import ThoughtBubble from "../../../public/thought-bubble.svg";
+
 export enum TalkStatusEnum {
   notListening = "not-listening",
   listening = "listening",
@@ -5,16 +10,52 @@ export enum TalkStatusEnum {
   talking = "talking",
 }
 
-export const getStatusMSG = (status: TalkStatusEnum) => {
+export const getStatusIconAndMsg = (status: TalkStatusEnum) => {
   switch (status) {
     case TalkStatusEnum.notListening:
-      return "Click here to start talking";
+      return (
+        <>
+          <div className=" text-black">Click here to start talking</div>
+          <MicrophoneDisabled className="h-12 w-12" />
+        </>
+      );
     case TalkStatusEnum.listening:
-      return "I am listening";
+      return (
+        <>
+          <div className=" text-black">I am listening</div>
+
+          <Microphone className="h-12 w-12" />
+        </>
+      );
     case TalkStatusEnum.thinking:
-      return "I am thinking, be patient";
+      return (
+        <>
+          <div className="text-black">I am thinking</div>
+          <Voice className="h-12 w-12 " />
+        </>
+      );
     case TalkStatusEnum.talking:
-      return "I am talking you better be listening";
+      return (
+        <>
+          <div className=" text-black">I am talking</div>
+          <ThoughtBubble className="h-12 w-12 " />
+        </>
+      );
+    default:
+      break;
+  }
+};
+
+const getStatusColorClass = (status: TalkStatusEnum) => {
+  switch (status) {
+    case TalkStatusEnum.notListening:
+      return "bg-white";
+    case TalkStatusEnum.listening:
+      return "bg-green-500";
+    case TalkStatusEnum.thinking:
+      return "bg-blue-500";
+    case TalkStatusEnum.talking:
+      return "bg-orange-500";
     default:
       break;
   }
@@ -28,8 +69,13 @@ export const TalkStatus = ({
   onClick: () => void;
 }) => {
   return (
-    <div className=" cursor-pointer" onClick={onClick}>
-      {getStatusMSG(status)}
+    <div
+      className={`${getStatusColorClass(
+        status
+      )} cursor-pointer w-[120px] h-[120px] flex justify-center flex-col items-center text-center rounded-lg text-gree`}
+      onClick={onClick}
+    >
+      {getStatusIconAndMsg(status)}
     </div>
   );
 };
