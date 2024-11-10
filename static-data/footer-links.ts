dfc8e7e45942393f3783f7d7fdfd75e954024ba5
Nikola Mitic <nikola.mitic.dev@gmail.com>
2024-11-10 14:36:04 +0100

    add dev to logo

--- static-data/footer-links.ts
+++ static-data/footer-links.ts
@@ -2,26 +2,32 @@ import SoLogoIcon from "../public/so-logo.svg?url";
 import CodepenLogoIcon from "../public/codepen-logo.svg?url";
 import GithubLogo from "../public/GitHub_Logo_White.png";
 import LinkedinLogo from "../public/LI-Logo.png";
+import DevToLogo from "../public/dev.to_logo.png"
 
 export const footerLinks = [
-    {
-      href: "https://github.com/nmitic",
-      logoAltText: "Github logo",
-      logo: GithubLogo,
-    },
-    {
-      href: "https://stackoverflow.com/users/5794392/nikola-mitic",
-      logoAltText: "stack overflow icon",
-      logo: SoLogoIcon,
-    },
-    {
-      href: "https://codepen.io/nikolamitic",
-      logoAltText: "CodepenLogoIcon",
-      logo: CodepenLogoIcon,
-    },
-    {
-      href: "https://www.linkedin.com/in/nikola-mitic-6a5b11119/",
-      logoAltText: "Linkedin logo",
-      logo: LinkedinLogo,
-    },
-  ]
+  {
+    href: "https://github.com/nmitic",
+    logoAltText: "Github logo",
+    logo: GithubLogo,
+  },
+  {
+    href: "https://dev.to/nmitic",
+    logoAltText: "Dev.to logo",
+    logo: DevToLogo,
+  },
+  {
+    href: "https://www.linkedin.com/in/nikola-mitic-6a5b11119/",
+    logoAltText: "Linkedin logo",
+    logo: LinkedinLogo,
+  },
+  {
+    href: "https://stackoverflow.com/users/5794392/nikola-mitic",
+    logoAltText: "stack overflow icon",
+    logo: SoLogoIcon,
+  },
+  {
+    href: "https://codepen.io/nikolamitic",
+    logoAltText: "CodepenLogoIcon",
+    logo: CodepenLogoIcon,
+  },
+]
